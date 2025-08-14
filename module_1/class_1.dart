fe13423970755b61945b381d33476a2fe8dec077
suyab-amin <suyabamins@gmail.com>
2025-08-15 03:05:59 +0600

    first class

---
+++ module_1/class_1.dart
@@ -0,0 +1,31 @@
+main(){
+  print('hello world');
+  int age=25;
+  print(age);
+  String Name="sunny";
+  print(Name);
+String fname="suyab";
+String lname="amin";
+
+
+//concate string
+  String Fullname=fname+" "+lname;
+   print(Fullname);
+
+  print("My name is $fname $lname");
+ 
+  double number=233.2;
+  bool islogin=true;
+  print(islogin);
+
+var test=10;
+print(test);
+//every data type is allow in dynamic ,like int ,strin,double
+dynamic test2=true;
+test2=10;
+test2='sa';
+test2=10.5;
+print(test2.runtimeType);
+
+}
+
