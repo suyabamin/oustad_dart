8da159f727350c2d54b2bb68cdac35a4610ec81b
suyab-amin <suyabamins@gmail.com>
2025-08-22 02:56:56 +0600

    list added

---
+++ module_2/class_1_list.dart
@@ -0,0 +1,13 @@
+main(){
+  List<int>numbers=[1,2,3,4,5];
+  print(numbers);
+  numbers.add(70);
+  print(numbers);
+  numbers.addAll([80,90,100]);
+   print(numbers);
+   numbers.insert(7, 75);
+      print(numbers);
+  numbers.insertAll(0, [10,20,30,40]);
+     print(numbers);
+
+}
