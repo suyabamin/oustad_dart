5bdaf6adc108cc861e115b0c76d6f61ffd58f424
suyab-amin <suyabamins@gmail.com>
2025-08-22 02:14:15 +0600

    class 3, basic

---
+++ module_1/class_3.dart
@@ -0,0 +1,50 @@
+main(){
+int a=20;
+int b=30;
+
+print(a+b);
+print(a-b);
+print(a/b);
+
+//increament
+
+print(a++);
+print(a);
+
+print('pre decreament');
+
+print(++a);
+print(a);
+
+print(a<b);
+print(b>a); 
+
+print(a<=b);
+print(b>=a); 
+
+int num=5;
+num =num+2;
+
+print(num);
+num=num*2;
+print(num);
+
+//type test
+var name='sunny';
+
+print(name is String);
+print(num is String);
+print(name is int);
+print(num is !int );
+
+print("njll safty");
+String name2="sunny";
+print(name2);
+
+
+String ? nickname;
+nickname='saaa';
+print(nickname);
+
+
+}
